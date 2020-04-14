e2a94f106602e8c90f92da687e73b83945c99150
taha uygun <tahauygun@gmail.com>
2020-04-13 23:49:07 -0700

    starting fashion class classification

--- fashion-class.py
+++ fashion-class.py
@@ -1,1 +1,7 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+fashion_train_df = pd.read_csv('input/fashion-mnist_train.csv', sep = ',')
+fashion_test_df = pd.read_csv('input/fashion-mnist_test.csv', sep = ',')
