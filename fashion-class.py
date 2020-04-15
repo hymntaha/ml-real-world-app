a8d7916c12103909c9b7c27e52df073066fa7896
taha uygun <tahauygun@gmail.com>
2020-04-14 23:24:01 -0700

    making selection more dynamic

--- fashion-class.py
+++ fashion-class.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import random
 
 fashion_train_df = pd.read_csv('./datasets/fashion-mnist_train.csv', sep = ',')
 fashion_test_df = pd.read_csv('./datasets/fashion-mnist_test.csv', sep = ',')
@@ -13,4 +14,5 @@ print(fashion_train_df.shape)
 training = np.array(fashion_train_df, dtype="float32")
 testing = np.array(fashion_test_df, dtype="float32")
 
-plt.imshow(training[10,1:])
+i = random.randint(1,60000)
+plt.imshow(training[i,1:].reshape(28,28))
