451b2fa66b370b7fc8439e57c1880f9b0be23964
taha uygun <tahauygun@gmail.com>
2020-04-14 23:04:05 -0700

    adding datasets for the model

--- fashion-class.py
+++ fashion-class.py
@@ -3,5 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-fashion_train_df = pd.read_csv('input/fashion-mnist_train.csv', sep = ',')
-fashion_test_df = pd.read_csv('input/fashion-mnist_test.csv', sep = ',')
+fashion_train_df = pd.read_csv('./datasets/fashion-mnist_train.csv', sep = ',')
+fashion_test_df = pd.read_csv('./datasets/fashion-mnist_test.csv', sep = ',')
+
+print(fashion_train_df.head())
