e73761d3a930ca2a624ffe879b236d7343f681af
taha uygun <tahauygun@gmail.com>
2020-04-20 23:13:25 -0700

    adding histograms

--- directing-subscription.py
+++ directing-subscription.py
@@ -14,3 +14,13 @@ dataset["hour"] = dateset.hour.str.slice(1,3).astype(int)
 
 dataset2 = dataset.copy().drop(columns = ['user','screen_list','enrolled_date','first_open','enrolled'])
 dataset2.head()
+
+plt.subtitle('Histograms of Numerical Columns', fontsize=20)
+for i in range(1, dataset2.shape[1]+1):
+    plt.subplots(3,3,i)
+    f = plt.gca()
+    f.set_title(dataset2.columns.values[i-1])
+
+    vals = np.size(dataset2.iloc[:, i-1].unique())
+    plt.hist(dataset2.iloc[:, i-1], bins = vals, color = '#3F5D7D' )
+
