002c88fc38deaedd866cb2a6ded70f9cae6a3172
taha uygun <tahauygun@gmail.com>
2020-05-02 23:37:41 -0700

    funnels continue

--- directing-subscription.py
+++ directing-subscription.py
@@ -74,3 +74,4 @@ dataset = dataset.drop(columns=["screen_list"])
 #funnels
 savings_screens = ["Saving1","Saving2","Saving2Amount","Saving4","Saving5","Saving6","Saving7","Saving8","Saving9","Saving10",]
 dataset['SavingsCount'] = dataset[savings_screens].sum(axis = 1)
+dataset = dataset.drop(columns=savings_screens)
