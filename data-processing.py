6a39c4497dc9e2e7808469eb573d9a0aac9ffbfd
taha uygun <tahauygun@gmail.com>
2020-05-07 23:30:46 -0700

    data preprocessing scaling

--- data-processing.py
+++ data-processing.py
@@ -13,3 +13,16 @@ dataset = dataset.drop(columns = 'enrolled')
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(dataset, response, test_size=0.2, random_state=0)
+
+train_identifier = X_train['user']
+x_train = X_train.drop(columns = 'user')
+test_identifier = X_test['user']
+X_test = X_test.drop(columns = 'user')
+
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train2 = pd.DataFrame(sc_X.fit_transform(X_train))
+X_test2 = pd.DataFrame(sc_X.transform(X_test))
+X_train2.columns = X_train.coluumns.values
+X_test2.columns = X_test.columns.values
+X_train2.index = X_train.index.values
