8430061df5e5e07c11b504067ef1e9f187f0b88c
taha uygun <tahauygun@gmail.com>
2020-05-07 23:26:49 -0700

    data processing test

--- data-processing.py
+++ data-processing.py
@@ -10,3 +10,6 @@ dataset = pd.read_csv('new_appdata10.csv')
 
 response = dataset["enrolled"]
 dataset = dataset.drop(columns = 'enrolled')
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(dataset, response, test_size=0.2, random_state=0)
